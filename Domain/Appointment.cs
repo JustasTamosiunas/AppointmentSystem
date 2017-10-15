7205d4b8ad0c40a478a479f41e304b9d793df245
Arnas Marozas <maraskaras123@gmail.com>
2017-10-15 21:43:15 +0300

    Created basic Domain classes for Appointment, User and Procedure

---
+++ Domain/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Appointment
+    {
+		public string UserID { get; set; }
+        public DateTime DateOfVisit { get; set; }
+        public List<string> ProcedureIdList { get; set; }
+		public string Remarks { get; set; }
+    }
+}
---
+++ Domain/Procedure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Procedure
+    {
+        public string Name { get; set; }
+		public DateTimeOffset Duration { get; set; }
+    }
+}
